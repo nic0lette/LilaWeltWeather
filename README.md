53373c5b62ea568cb58596b63e1f35aad1c56c10
Nikki Borrelli <nicole@hitori.org>
2023-08-23 08:00:51 +0100

    Better geocoding, etc...

--- README.md
+++ README.md
@@ -4,5 +4,17 @@ MQTT Weather forecast API
 
 ## Usage
 
-To use this, create a file named `config.py` and set the value of `USER_AGENT` based on the input
-from https://api.met.no/doc/FAQ.
+To use this, create a file named `config.py` in the form:
+
+```python
+USER_AGENT = "User-Agent for met.no (email/project/etc)"
+
+MQTT = {
+    "broker": "server.address",
+    "port": 1234,
+    "user": "user",
+    "password": "password"
+}
+```
+
+Guidance for the value of `USER_AGENT` can be found on https://api.met.no/doc/FAQ.
--- main.py
+++ main.py
@@ -1,63 +1,142 @@
-from collections import namedtuple
+import json
 from datetime import timedelta
 from threading import Lock
+from types import SimpleNamespace
 
+import paho.mqtt.client as mqtt
 import requests
-from cachetools import LRUCache, cached, TTLCache
+from cachetools import cached, TTLCache, LRUCache
+from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
+
+from config import MQTT
 from config import USER_AGENT
 
 geocoder_url = "https://geocode.maps.co/search"
 weather_url = "https://api.met.no/weatherapi/locationforecast/2.0/complete.json"
 
-headers = {
-    "User-Agent": USER_AGENT
-}
-
 # Global shared instance for converting lat/lon to a timezone
 timezone_finder = TimezoneFinder()
 
-# Type for passing around place data (lat, lon, and timezone)
-PlaceData = namedtuple("PlaceData", "lat lon tz")
+# Similar for geocoding
+geolocator = Nominatim(user_agent=USER_AGENT)
 
+# Cache for place -> location lookups
+geocode_cache = {}
 
-@cached(cache=LRUCache(maxsize=1000), lock=Lock())
-def geocode_place(place: str) -> PlaceData:
-    parameters = {
-        "q": {place}
-    }
-    location_response = requests.get(geocoder_url, params=parameters, headers=headers).json()[0]
-    lat = location_response["lat"]
-    lon = location_response["lon"]
-    tz = timezone_finder.timezone_at(lng=float(lon), lat=float(lat))
-    return PlaceData(lat=lat, lon=lon, tz=tz)
+# Cache for reverse geocaching
+rev_geocode_cache = LRUCache(1000)
 
 
 @cached(cache=TTLCache(100, ttl=timedelta(minutes=10).seconds), lock=Lock())
-def get_forecast_(lat: str, lon: str):
+def get_forecast_(lat: float, lon: float):
     try:
         parameters = {
             "lat": {lat},
             "lon": {lon}
         }
-        forecast = requests.get(weather_url, params=parameters, headers=headers).json()
-        return forecast
-    except Exception:
+        headers = {
+            "User-Agent": USER_AGENT
+        }
+
+        return requests.get(weather_url, params=parameters, headers=headers).json()
+    except Exception as ex:
+        print(f"Failed to get forecast: {ex}")
         return None
 
 
-def get_forecast(location: PlaceData):
+def geocode_place(place: str):
+    cache_key = place.lower()
+    if cache_key not in geocode_cache:
+        location = geolocator.geocode(place)
+        geocode_cache[cache_key] = location
+        print(f"Geocode miss for '{place}': {location}")
+    return geocode_cache[cache_key]
+
+
+def rev_geocode(lat: float, lon: float):
+    cache_key = f"{lat:.3f},{lon:.3f}"
+    if cache_key not in rev_geocode_cache:
+        location = geolocator.reverse(f"{lat}, {lon}")
+        rev_geocode_cache[cache_key] = location
+        print(f"Reverse geocode miss for {lat}, {lon}: {location}")
+    return rev_geocode_cache[cache_key]
+
+
+def get_forecast(lat: float, lon: float):
     # For some reason PyCharm thinks that calling a method with a `@cached` decorator (when it has more than one
     # parameter?) is "calling" it? Until this is fixed, suppress the warning here.
     # Issue: https://youtrack.jetbrains.com/issue/PY-52210
     # noinspection PyCallingNonCallable
-    return get_forecast_(location.lat, location.lon)
+    return get_forecast_(lat, lon)
+
+
+def place_request(place: str) -> dict:
+    location = geocode_place(place)
+    lat = float(location.latitude)
+    lon = float(location.longitude)
+    timezone = timezone_finder.timezone_at(lat=lat, lng=lon)
+    forecast = get_forecast(lat, lon)
+    return {
+        "location": location.raw,
+        "timezone": timezone,
+        "forecast": forecast,
+    }
+
+
+def point_request(lat: float, lon: float) -> dict:
+    location = rev_geocode(lat, lon)
+    timezone = timezone_finder.timezone_at(lat=lat, lng=lon)
+    forecast = get_forecast(lat, lon)
+    return {
+        "location": location.raw,
+        "timezone": timezone,
+        "forecast": forecast,
+    }
+
+
+def on_connect(client, userdata, flags, rc):
+    # This will be called once the client connects
+    print(f"Connected with result code {rc}")
+    # MQTT isn't really an RPC framework, so we use two topics
+    topic = MQTT["topic"]
+    client.subscribe(f"{topic}/request")
+
+
+def on_message(client, userdata, msg):
+    topic = MQTT["topic"]
+    print(f"MQTT request received [{msg.topic}]: {msg.payload}")
+    try:
+        req = json.loads(msg.payload, object_hook=lambda d: SimpleNamespace(**d))
+    except json.JSONDecodeError as ex:
+        print(f"Error decoding request: {ex}")
+        error = {
+            "payload": f"Error parsing payload: {msg.payload}"
+        }
+        client.publish(f"{topic}/response", payload=json.dumps(error))
+        return
+
+    if hasattr(req, "place"):
+        print(f"Place lookup: {req.place}")
+        response = place_request(req.place)
+    elif hasattr(req, "lat") and hasattr(req, "lon"):
+        print(f"Lat/lon lookup: {req.lat}, {req.lon}")
+        response = point_request(req.lat, req.lon)
+    else:
+        print(f"Unknown request: {req}")
+        response = {
+            "type": "error"
+        }
+    client.publish(f"{topic}/response", payload=json.dumps(response))
 
 
 def main():
-    tokyo = geocode_place("Tokyo")
-    forecast = get_forecast(location=tokyo)
-    print(f"Forecast: {forecast}")
+    client = mqtt.Client(MQTT["client_id"])
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set(MQTT["user"], MQTT["password"])
+    client.connect(MQTT["broker"], MQTT["port"])
+    client.loop_forever()  # Start networking daemon
 
 
 # Press the green button in the gutter to run the script.
